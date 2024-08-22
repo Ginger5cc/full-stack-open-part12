117ac998836bd647b69ea194dafe914007450379
Ginger <gingerginger5cc@gmail.com>
2024-08-22 13:11:44 -0400

    ex12.19 - connect frontend and backend to reverse proxy nginx

--- todo-app/todo-frontend/src/Todos/TodoView.jsx
+++ todo-app/todo-frontend/src/Todos/TodoView.jsx
@@ -36,7 +36,7 @@ const TodoView = () => {
 
   return (
     <>
-      <h1>Todos I do not know what is going on?</h1>
+      <h1>Todos what is going on?</h1>
       <Form createTodo={createTodo} />
       <List todos={todos} deleteTodo={deleteTodo} completeTodo={completeTodo} />
     </>
